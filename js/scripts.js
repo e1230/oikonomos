0c90d610204a8168c0c65e412f62d40a54a2a8a9
e1320 <emaurobel1320@hotmail.com>
2021-02-18 15:10:23 -0500

    version 1

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+function formularioAparece(){
+        $("#form-credito").collapse('show');
+}
+$(document).ready(function(){
+        $(".btn-primary").click(function(){
+                //$("#barra-carga").collapse('show');
+                animar();
+                setTimeout(formularioAparece,4000);
+                $('#boton-buscar').hide();
+                $('#slc').hide();
+        });
+        $("#slc").change(function() {
+                $( "#boton-buscar" ).prop( "disabled", false );
+        });
+        $("#form2").click(function(){
+                $("#formulario").collapse('show');
+        });
+});
+
+
+$('.carousel').carousel({
+        interval: 2500
+})
+
+function animar(){
+        document.getElementById("barra-carga").classList.toggle("final");
+}
+
